296e0c820f8ad6b109246dba2ab43b40e066ad9c
Raphael Krupinski <rafalkrupinski@users.noreply.github.com>
2019-05-04 20:41:37 +0200

    Fix version and test

--- tests/allegro_pl/test_init.py
+++ tests/allegro_pl/test_init.py
@@ -1,10 +1,14 @@
+from unittest import TestCase
+
 import allegro_pl
 import toml
 
 
-def test_version():
-    def get_project_version(self):
-        return toml.load('pyproject.toml')['tool']['poetry']['version']
+class test_init(TestCase):
 
     def test_version_matches(self):
-        self.assertEqual(self.get_project_version(), allegro_pl.__version__)
+        self.assertEqual(get_project_version(), allegro_pl.__version__)
+
+
+def get_project_version():
+    return toml.load('pyproject.toml')['tool']['poetry']['version']
